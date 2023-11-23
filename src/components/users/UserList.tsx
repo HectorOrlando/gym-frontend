246507b68336c8e4443502ec7fd260f142cbd693
HectorOrlando <14orgui@gmail.com>
2023-11-23 20:19:47 +0100

    implement table crud

--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -4,9 +4,18 @@ import { useContext } from 'react';
 import { UserContext } from './UserContext';
 import { UserAction, createUser } from '../types';
 
-import { Button, List, ListItem } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import { Button, ButtonGroup } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import EditIcon from '@mui/icons-material/Edit';
 
 const UserList: React.FC = () => {
 
@@ -28,25 +37,41 @@ const UserList: React.FC = () => {
         dispatch(action);
     }
 
+    const handleUpdateUser = (userId: string) => {
+        console.log(`actualizar usuario nº :  ${userId}`);
+    }
 
     return (
-        <List sx={{ width: '100%', maxWidth: 660, bgcolor: 'background.paper' }}>
-            {
-                users.map(({ id, name, email }) => (
-                    <ListItem key={id} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <div>
-                            {`Id:   ${id}   Name:   ${name} Email:  ${email}`}
-                        </div>
-                        <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => handleRemoveUser(id)}>
-                            Delete
-                        </Button>
-                    </ListItem>
-                ))
-            }
-            <Button variant="contained" color="success" startIcon={<AddCircleIcon />} onClick={() => handleAddUser()}>
-                Add User
-            </Button>
-        </List>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="left">ID</TableCell>
+                        <TableCell align="left">Name</TableCell>
+                        <TableCell align="left">Email</TableCell>
+                        <TableCell align="center">Action</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        users.map(({ id, name, email }) => (
+                            <TableRow key={id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell component="th" scope="row">{id}</TableCell>
+                                <TableCell align="left">{name}</TableCell>
+                                <TableCell align="left">{email}</TableCell>
+                                <TableCell align="center" sx={{ display: 'flex', justifyContent: 'center' }}>
+                                    <ButtonGroup variant="outlined" aria-label="outlined button group">
+                                        <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => handleRemoveUser(id)}></Button>
+                                        <Button variant="outlined" startIcon={<EditIcon />} onClick={() => handleUpdateUser(id)}></Button>
+                                        <Button variant="contained" startIcon={<AddCircleIcon />} onClick={() => handleAddUser()}></Button>
+                                    </ButtonGroup>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
