6c2133e8a5837cda56e075f709ebff5d5487a9a6
HectorOrlando <14orgui@gmail.com>
2023-11-23 20:52:27 +0100

    change name pages

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,7 +7,7 @@ const UsersPage = () => {
 
     return (
         <Layout title="Users - Gym">
-            <h1>Users Page</h1>
+            <h1>Users List</h1>
             {/* Agrega aquí el contenido de la página Users */}
             <UserList />
         </Layout>
